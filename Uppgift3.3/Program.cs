e2f1ee2b845241bec2172d285155fa67730455c2
ercr07001 <ercr07001@E-PW05CGR0>
2024-11-07 13:11:47 +0100

    Add project files.

---
+++ Uppgift3.3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.Design;
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("För hur många timmar vill du hyra ut bilen?");
+            Console.WriteLine("MAX: 12 timmar!");
+           
+            int timmar = int.Parse(Console.ReadLine());
+
+            if (timmar <= 12)
+            {
+                Console.WriteLine("Du får hrya bilen");
+            }
+            else if (timmar > 12)
+            {
+                Console.WriteLine("Du får inte hyra bilen");
+            }
+           
+
+            int nr = 80;
+            int nr2 = timmar * nr;
+
+            Console.WriteLine("du vill hyra ut så här länge:" + " " + timmar + "h" + " " + "Det kommer att kosta dig:" + " " + nr2 + "kr");
+            Console.WriteLine("Ha en trevlig resa nu.");
+
+            Console.ReadLine();
+        }
+    }
+}
